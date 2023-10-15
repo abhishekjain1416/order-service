e183a1fd4ebc30ecc1b51bca489557ff01ff31ff
Abhishek Jain <abhishek.jain@docquity.com>
2023-10-15 13:05:17 +0530

    Feat: Order Service: Place Order Online from App

---
+++ src/main/java/com/programmingtechie/orderservice/dto/OrderRequest.java
@@ -0,0 +1,16 @@
+package com.programmingtechie.orderservice.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
