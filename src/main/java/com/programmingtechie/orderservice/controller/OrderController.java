a95ff44cd8e0eec8b8c5f9f1474e32f96560269f
Abhishek Jain <abhishekjain1416@gmail.com>
2023-10-29 13:50:25 +0530

    Feat: Circuit Breaker, Time Limiter, Retry Events

--- src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.programmingtechie.orderservice.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
@@ -20,9 +26,17 @@ public class OrderController {
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
         
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully.";
+        return CompletableFuture.supplyAsync(() -> 
+            orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() -> 
+            "Oops! Something went wrong, please order after some time!");
     }
 }
