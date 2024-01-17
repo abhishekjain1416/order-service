77a139da5c28c947ad14995fb8295317d75eb37f
Abhishek Jain <abhishekjain1416@gmail.com>
2024-01-17 14:40:47 +0530

    Strategy Design Pattern to achieve loose coupling

--- src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,79 +1,11 @@
 package com.programmingtechie.orderservice.service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import com.programmingtechie.orderservice.dto.InventoryResponse;
-import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
-import com.programmingtechie.orderservice.event.OrderPlacedEvent;
-import com.programmingtechie.orderservice.model.Order;
-import com.programmingtechie.orderservice.model.OrderLineItems;
-import com.programmingtechie.orderservice.repository.OrderRepository;
 
 @Service
-public class OrderService {
+public interface OrderService {
 
-    @Autowired
-    public OrderRepository orderRepository;
-
-    @Autowired
-    public WebClient.Builder webClientBuilder;
-
-    @Autowired
-    private KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
-
-    public String placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-
-        // Map OrderLineItemsDto to OrderLineItems and collect them into a list
-        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
-            .stream()
-            .map(this::mapToDto)
-            .toList();
-
-        order.setOrderLineItemsList(orderLineItems);
-
-        List<String> skuCodes = order.getOrderLineItemsList()
-            .stream()
-            .map(OrderLineItems::getSkuCode)
-            .toList();
-
-        // Call Inventory Service to check if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
-            .uri("http://inventory-service/api/inventory",
-                uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-            .retrieve()
-            .bodyToMono(InventoryResponse[].class)
-            .block();
-
-        // Check if all products are in stock
-        Boolean allProductsInStock = Arrays.stream(inventoryResponsesArray)
-            .allMatch(InventoryResponse::getIsInStock);
-
-        // If all products are in stock, save the order and send a notification
-        if(allProductsInStock) {
-            orderRepository.save(order);
-            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
-            return "Order Placed Successfully.";
-        }
-        else {
-            throw new IllegalArgumentException("Products are not in stock, please try again later.");
-        }
-    }
-
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
-        OrderLineItems orderLineItems = new OrderLineItems();
-        orderLineItems.setPrice(orderLineItemsDto.getPrice());
-        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
-        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
-        return orderLineItems;
-    }
+    String placeOrder(OrderRequest orderRequest);
 }
